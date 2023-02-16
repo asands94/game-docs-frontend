74a62d80185e14d945a16885ecfb5104471c57db
asands94 <ashley.sands01@gmail.com>
2023-02-16 09:51:37 -0500

    change port num

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,8 +1,7 @@
 let apiUrl
 const apiUrls = {
-    // YOU MUST CHANGE PRODUCTION URL WHEN DEPLOYING
 	production: process.env.REACT_APP_PROD_URL,
-	development: 'http://localhost:8000',
+	development: 'http://localhost:8080',
 }
 
 if (window.location.hostname === 'localhost') {
